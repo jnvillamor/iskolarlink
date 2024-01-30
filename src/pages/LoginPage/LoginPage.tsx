8d3abd699f247f2abfd683b06a4b7c8d6982412a
-m <villamorjohnnoel@gmail.com>
2024-01-30 22:04:53 +0800

    Create LoginPage and initialize its route

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,22 +1,9 @@
-import { useState } from 'react';
+const LoginPageComponent = () => {
+  return <div>Login Page</div>;
+};
 
 const LoginPage = () => {
-  return (
-    <>
-      <div>
-        <h1>Register</h1>
-        <div>
-          <label htmlFor='email'>Email</label> <br />
-          <input type='text' name='email' id='email' className='border border-black'/>
-        </div>
-        <div>
-          <label htmlFor='password'>Password</label> <br />
-          <input type='password' name='password' id='password' className='border border-black' />
-        </div>
-        <button className='border p-3 bg-slate-400'>Register</button>
-      </div>
-    </>
-  );
+  return <LoginPageComponent />;
 };
 
 export default LoginPage;
